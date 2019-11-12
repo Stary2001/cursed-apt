2909826626f46b0a1cc6dc3be4f2a9fee5728313
Ezekiel Bethel <zek@9net.org>
2019-11-12 15:47:37 +0000

    .

--- create-overlay.sh
+++ create-overlay.sh
@@ -1,7 +1,9 @@
-mkdir -p overlay/changes/var/lib/dpkg/ overlay/changes/var/cache/apt/archives/ overlay/changes/var/lib/dpkg/triggers
+mkdir -p overlay/changes/var/lib/dpkg/ overlay/changes/var/cache/apt/archives/ overlay/changes/var/lib/dpkg/triggers overlay/changes/var/log/apt/ overlay/changes/var/cache/debconf/
 mkdir -p overlay/work
 
 touch overlay/changes/var/lib/dpkg/lock-frontend
 touch overlay/changes/var/lib/dpkg/lock
 touch overlay/changes/var/cache/apt/archives/lock
 touch overlay/changes/var/lib/dpkg/triggers/Lock
+touch overlay/changes/var/cache/debconf/passwords.dat # this shuts up dpkg
+touch overlay/changes/var/log/apt/term.log # this also shuts up dpkg
